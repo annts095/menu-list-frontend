e21fbbc8feb414de4be198b3ab481e7ab029bcfe
anan-tasuku <anantasuku@team-lab.com>
2022-01-20 01:17:45 +0900

    feat 有効な親メニュー一覧を画面に表示

--- menu-list-frontend/pages/index.tsx
+++ menu-list-frontend/pages/index.tsx
@@ -4,21 +4,39 @@ import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch } from '../redux/store'
 import { useEffect } from 'react'
 import { categoriesSelector } from '../redux/selector/category'
+import { fetchMenus } from '../redux/modules/menu'
+import { menusSelector } from '../redux/selector/menu'
 
 const Home: NextPage = () => {
   const dispatch: AppDispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCategories());
+    dispatch(fetchMenus());
   }, []);
   const categories = useSelector(categoriesSelector);
+  const menus = useSelector(menusSelector);
   return (
-    <ul>
-      <p>すべて</p>
-      <p>おすすめ</p>
-      {categories.map((category) => (
-        <p key={category.sort_number}>{category.title}</p>
-      ))}
-    </ul>
+    <>
+      <div>
+        <p>カテゴリ</p>
+        <ul>
+          <li>すべて</li>
+          <li>おすすめ</li>
+          {categories.map((category) => (
+            <li key={category.sort_number}>{category.title}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <p>メニュー</p>
+        <ul>
+          {menus.map((menu) => (
+            <li key={menu.id}>{menu.title} : {menu.price}円</li>
+          ))}
+        </ul>
+      </div>
+
+    </>
   )
 }
 
