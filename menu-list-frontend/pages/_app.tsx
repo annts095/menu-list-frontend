e2cbf9b47faa230525fc26f8aa4523e8bb5cfdfa
adust09 <shoukitsuda@icloud.com>
2022-01-20 14:41:11 +0900

    conponentsの配置を変更

--- menu-list-frontend/pages/_app.tsx
+++ menu-list-frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Card  from "./components/card";
+import Card  from "../components/card";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
