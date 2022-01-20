30f677a9e0bbdb9f0d7be322dc1f36de4a4824fe
anan-tasuku <anantasuku@team-lab.com>
2022-01-21 00:34:14 +0900

    fix カードにmenuを渡す

--- menu-list-frontend/components/card.tsx
+++ menu-list-frontend/components/card.tsx
@@ -1,17 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
+import { ConvertedMenu } from '../declarations/menu';
 import ImageModal  from "./modal";
 
-export default function Cards() {
+type Props = {
+    menu: ConvertedMenu;
+};
+
+export default function Card({ menu }: Props) {
     return (
         <div className="row row-cols-1 row-cols-md-3 g-4">
             <div className="col">
                 <div className="card h-100" >
-                    <img src="karamen.JPG" className="card-img-top" alt="..." />
+                    { menu.image_url !== null && <img src={menu.image_url} className="card-img-top" alt="..." /> }
                     <ImageModal />
                     <div className="card-body">
-                        <h5 className="card-title">辛麺</h5>
-                        <p className="card-text"> 価格:600円</p>
+                        <h5 className="card-title">{ menu.title }</h5>
+                        <p className="card-text"> 価格:{ menu.price }円</p>
                     </div>
                 </div>
             </div>
